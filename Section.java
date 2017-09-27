64d519ab357886f8bf6163f9e1bbb22aa17ee04e
Saldyus <dinaro.scuola@gmail.com>
2017-09-27 17:59:45 +0200

    Update Section.java
    Added addParam(), removeParam(), and getParam() methods.

--- Section.java
+++ Section.java
@@ -16,4 +16,35 @@ public class Section {
     public String getName(){
         return name;
     }
+    
+    public void addParam(String name, String value){
+        parameter.add(name+"="+value);
+    }
+    
+    public void removeParam(String name){
+        for(int i=0; i<parameter.size(); i++){
+            String param=parameter.get(i);
+            for(int j=0; j<param.length(); j++){
+                if(param.charAt(j)=='='){
+                    if(param.substring(0, j).equals(name)){
+                        parameter.remove(i);
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    
+    public String getParam(String name){
+        for(int i=0; i<parameter.size(); i++){
+            for(int j=0; j<parameter.get(i).length(); j++){
+                if(parameter.get(i).charAt(j)=='='){
+                    return parameter.get(i).substring(j+1);
+                }
+            }
+        }
+        return null;
+    }
+    
 }
