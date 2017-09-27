a42bd5bcd997a8763b46fc55dfd74c601b782f2a
Saldyus <dinaro.scuola@gmail.com>
2017-09-27 16:00:44 +0200

    Create Section.java
    add methods getName()

---
+++ Section.java
@@ -0,0 +1,19 @@
+package Manage_File_INI;
+
+/**
+ *
+ * @author Abbadati Alessio & Dinaro Salvatore
+ */
+
+public class Section {
+    
+    private String name;
+    
+    public Section(String name){
+        this.name=name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+}
