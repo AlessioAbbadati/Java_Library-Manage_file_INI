8db5a5f886f3f79abfefbe86ee7fa53520ec8501
Saldyus <dinaro.scuola@gmail.com>
2017-09-30 10:36:19 +0200

    Update Test_Config.java

--- test/Test_Config.java
+++ test/Test_Config.java
@@ -1,18 +1,20 @@
-import Manage_File_INI.Section;
+import Manage_File_INI.Config;
+import Manage_File_INI.SectionAlreadyExistException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 /**
  *
- * @author Abbadaati Alessio & Dinaro Salvatore
+ * @author Abbadati Alessio & Dinaro Salvatore
  */
-public class TestSection {
+public class Test_Config {
     
-    public TestSection() {
+    public Test_Config() {
     }
     
     @BeforeClass
@@ -36,12 +38,50 @@ public class TestSection {
     //
     // @Test
     // public void hello() {}
+    
+    /*
+    *
+    *
+    */
     @Test
-    public void testAddRemoveParam(){
-        Section s=new Section("Section1");
-        s.addParam("Param1", "25");
-        assertEquals("25", s.getParam("Param1"));
-        s.removeParam("Param1");
-        assertEquals(null, s.getParam("Param1"));
-    }    
+    public void testAddSection(){
+        boolean exception1=false;
+        boolean exception2=false;
+        boolean exception3=false;
+        
+        Config c=new Config();
+        try {
+            c.addSection("Section1");
+        } catch (SectionAlreadyExistException ex) {exception1=true;}
+        assertEquals(true,c.ExistSection("Section1"));
+        assertFalse(exception1);
+        
+        try {
+            c.addSection("Section1");
+        } catch (SectionAlreadyExistException ex) {exception2=true;}
+        assertFalse(exception2);
+        
+        try {
+            c.addSection("Section2");
+        } catch (SectionAlreadyExistException ex) {exception3=true}
+        assertEquals(true,c.ExistSection("Section2"));
+        assertFalse(exception3);
+    }
+    
+    @Test
+    public void testRemoveSection() throws SectionAlreadyExistException{
+        Config c=new Config();
+        c.addSection("Section1");
+        assertEquals(true,c.ExistSection("Section1"));
+        c.removeSection("Section1");
+        assertEquals(false,c.ExistSection("Section1"));
+    }
+    @Test
+    public void testAddParam() throws SectionAlreadyExistException{
+        Config c=new Config();
+        c.addSection("Section1");
+        assertEquals(true,c.ExistSection("Section1"));
+        c.addParam("Section1", "Param1", "25");
+        assertEquals("25",c.getParam("Section1", "Param1"));
+    }
 }
